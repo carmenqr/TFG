711234e3c9094776ca5f96dcb60cb5a2714105dd
carmenqr <e.carmenquilesr@go.ugr.es>
2025-05-07 10:14:20 +0200

    Eliminación carpetas varias

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "🚀 Desplegando la app en ShinyApps.io..."
-rsconnect deploy shiny /home/usuario/Documentos/TFG --name tfgcarmen --title app.py
-echo "✅ ¡App actualizada con éxito!"
